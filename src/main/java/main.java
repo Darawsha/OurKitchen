95cbe876c72e8159dc27a84b3781e08785fac152
Ameed Darawsheh <s12240153@stu.najah.edu>
2025-02-15 16:04:03 +0200

    apply Javafx

---
+++ src/main/java/main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class main extends Application {
+    @Override
+    public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+			Scene  scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
