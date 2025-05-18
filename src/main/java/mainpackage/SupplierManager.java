b5551be2c9a7e232a4a03ef3eb5d47fd82084fdc
Ameed Darawsheh <ce.ameed@gmail.com>
2025-05-18 23:30:44 +0300

    Do Supplier Integration Steps

--- src/main/java/mainpackage/SupplierManager.java
+++ src/main/java/mainpackage/SupplierManager.java
@@ -1,4 +1,39 @@
 package mainpackage;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SupplierManager {
-	
+
+	private Map<String, Double> ingredientPrices;
+
+	public SupplierManager() {
+
+		ingredientPrices = new HashMap<>();
+		ingredientPrices.put("Tomato", 1.25);
+		ingredientPrices.put("Cheese", 2.50);
+		ingredientPrices.put("Lettuce", 0.80);
+	}
+
+	public String getRealTimePrice(String ingredientName) {
+		if (ingredientPrices.containsKey(ingredientName)) {
+			return ingredientName + " price: $" + ingredientPrices.get(ingredientName);
+		} else {
+			return ingredientName + " not available";
+		}
+	}
+
+	public boolean sendOrder(PurchaseOrder order) {
+		System.out.println("Sending order to supplier: " + order.getItemName() + ", Quantity: " + order.getQuantity());
+
+		return true;
+
+	}
+
+	public boolean connect() {
+
+		return true;
+
+	}
+
 }
